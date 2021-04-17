4980ceba2f5ac0e55a88a7ed180e378cdf77c0ca
Mergen Hoja <mergendz@gmail.com>
2021-04-16 17:13:45 -0700

    Initial commit - Spring Boot Project

---
+++ src/main/java/com/carpetcalculator/CarpetCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.carpetcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpetCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpetCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carpetcalculator/CarpetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpetcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpetCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
