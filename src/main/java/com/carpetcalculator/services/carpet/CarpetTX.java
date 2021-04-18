743ca4e44812a33e133499299a4f3b0a68e7a39b
Mergen Hoja <mergendz@gmail.com>
2021-04-18 11:17:35 -0700

    Added implementing Carpet classes for TX and VA

--- src/main/java/com/carpetcalculator/services/carpet/CarpetTX.java
+++ src/main/java/com/carpetcalculator/services/carpet/CarpetTX.java
@@ -2,12 +2,35 @@ package com.carpetcalculator.services.carpet;
 
 import com.carpetcalculator.enums.City;
 import com.carpetcalculator.interfaces.carpetPrices.Carpet;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
+@Component
 public class CarpetTX implements Carpet {
+
+    private static final Map<City,BigDecimal> mapOfUnitPricesPerCity = new HashMap<>();
+
+    static {
+        mapOfUnitPricesPerCity.put(City.AUSTIN, new BigDecimal("1.32"));
+        mapOfUnitPricesPerCity.put(City.DALLAS, new BigDecimal("1.75"));
+        mapOfUnitPricesPerCity.put(City.SAN_ANTONIO, new BigDecimal("2.20"));
+    }
+
+
     @Override
     public BigDecimal getSqFtPrice(City city) {
-        return null;
+
+        BigDecimal defaultValue = BigDecimal.ZERO;
+
+        // Get the map
+        Optional<Map.Entry<City, BigDecimal>> filteredMap = mapOfUnitPricesPerCity.entrySet().stream().
+                filter(x -> x.getKey() == city).findFirst();
+
+        // If there is a value then return, otherwise return the default value
+        return filteredMap.isPresent() ? filteredMap.get().getValue() : defaultValue;
     }
 }
--- src/main/java/com/carpetcalculator/services/carpet/CarpetVA.java
+++ src/main/java/com/carpetcalculator/services/carpet/CarpetVA.java
@@ -2,12 +2,35 @@ package com.carpetcalculator.services.carpet;
 
 import com.carpetcalculator.enums.City;
 import com.carpetcalculator.interfaces.carpetPrices.Carpet;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
+@Component
 public class CarpetVA implements Carpet {
+
+    private static final Map<City,BigDecimal> mapOfUnitPricesPerCity = new HashMap<>();
+
+    static {
+        mapOfUnitPricesPerCity.put(City.MCLEAN, new BigDecimal("4.32"));
+        mapOfUnitPricesPerCity.put(City.ARLINGTON, new BigDecimal("2.92"));
+        mapOfUnitPricesPerCity.put(City.FAIRFAX, new BigDecimal("4.65"));
+    }
+
+
     @Override
     public BigDecimal getSqFtPrice(City city) {
-        return null;
+
+        BigDecimal defaultValue = BigDecimal.ZERO;
+
+        // Get the map
+        Optional <Map.Entry<City, BigDecimal>> filteredMap = mapOfUnitPricesPerCity.entrySet().stream().
+                                                            filter(x -> x.getKey() == city).findFirst();
+
+        // If there is a value then return, otherwise return the default value
+        return filteredMap.isPresent() ? filteredMap.get().getValue() : defaultValue;
     }
 }
